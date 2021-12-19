d01bd1091e70da8256a3f6cfdf741acfe267353b
jan <janek.grochulski@gmail.com>
2021-12-18 22:50:52 -0600

    ok

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Triangle
   # write code here
+  def initialize s1, s2, s3
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  class TriangleError < StandardError
+    def message 
+      "Invalid geometry. Check triangle side inputs."
+    end
+  end
+
+
+  def kind
+    arr = [@s1, @s2, @s3]
+    # binding.pry
+
+    if (arr.include? 0) || (@s1 >= @s2 + @s3 || @s2 >= @s1 + @s3 || @s3 >= @s1 + @s2) || (arr.filter {|num| num < 0 }.length > 0)
+      raise TriangleError
+    else
+      if arr.uniq.length == 1
+        return :equilateral
+      elsif arr.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    end
+  end
+
 end
+
+t1 = Triangle.new(4, 4, -4)
+
+# binding.pry
+
